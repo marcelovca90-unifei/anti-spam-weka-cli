070ec21d365051066a37853d8a792ece7a566d98
Marcelo Cysneiros <marcelovca90@gmail.com>
2018-03-31 01:37:22 -0300

    Save t-SNE plots to data set folders

--- AntiSpamWekaCLI/src/main/java/io/github/marcelovca90/analysis/TsneAnalyser.java
+++ AntiSpamWekaCLI/src/main/java/io/github/marcelovca90/analysis/TsneAnalyser.java
@@ -1,8 +1,13 @@
 package io.github.marcelovca90.analysis;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.math.plot.FrameView;
 import org.math.plot.Plot2DPanel;
 import org.math.plot.PlotPanel;
@@ -14,6 +19,7 @@ import com.jujutsu.tsne.barneshut.BarnesHutTSne;
 import com.jujutsu.tsne.barneshut.ParallelBHTsne;
 import com.jujutsu.utils.TSneUtils;
 
+import io.github.marcelovca90.classification.ClassifierBuilder;
 import io.github.marcelovca90.data.ClassType;
 import io.github.marcelovca90.data.DatasetMetadata;
 import weka.core.Instance;
@@ -21,6 +27,8 @@ import weka.core.Instances;
 
 public class TsneAnalyser
 {
+    private static final Logger LOGGER = LogManager.getLogger(ClassifierBuilder.class);
+
     private double[][] X;
     private ClassType[] labels;
     private int hamCount;
@@ -47,8 +55,7 @@ public class TsneAnalyser
         // run t-SNE
         double[][] Y = tsne.tsne(config);
 
-        // plot Y (i.e. the result of t-SNE)
-
+        // prepare data for plotting
         double[][] ham = new double[hamCount][];
         String[] hamNames = new String[hamCount];
         double[][] spam = new double[spamCount][];
@@ -70,23 +77,39 @@ public class TsneAnalyser
             }
         }
 
+        // create parent plot panel
         Plot2DPanel plot = new Plot2DPanel();
         plot.addLegend("EAST");
 
+        // prepare ham plot
         ScatterPlot hamPlot = new ScatterPlot("ham", PlotPanel.COLORLIST[0], ham);
         hamPlot.setTags(hamNames);
 
+        // prepare spam plot
         ScatterPlot spamPlot = new ScatterPlot("spam", PlotPanel.COLORLIST[1], spam);
         spamPlot.setTags(spamNames);
 
+        // add plots to canvas
         plot.plotCanvas.setNotable(true);
         plot.plotCanvas.setNoteCoords(true);
         plot.plotCanvas.addPlot(hamPlot);
         plot.plotCanvas.addPlot(spamPlot);
 
+        // display plot frame
         FrameView plotframe = new FrameView(plot);
         plotframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        plotframe.setVisible(true);
+        plotframe.setVisible(false);
+
+        // save plot image to filesystem
+        File outputFile = new File(metadata.getArffFilename().replace("data.arff", "t-SNE.png"));
+        try
+        {
+            plot.toGraphicFile(outputFile);
+        }
+        catch (IOException e)
+        {
+            LOGGER.error("Could not save graphic file " + outputFile.getAbsolutePath() + ".", e);
+        }
     }
 
     private void prepareInputAndLabels(Instances dataset)
