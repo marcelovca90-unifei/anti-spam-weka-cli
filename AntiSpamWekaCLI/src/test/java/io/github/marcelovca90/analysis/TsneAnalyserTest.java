d90df379a2422a3b8586e58f45f8c8023b48a836
Marcelo Cysneiros <marcelovca90@gmail.com>
2018-03-31 15:07:32 -0300

    Fix and improve TsneAnalyser unit tests coverage

--- AntiSpamWekaCLI/src/test/java/io/github/marcelovca90/analysis/TsneAnalyserTest.java
+++ AntiSpamWekaCLI/src/test/java/io/github/marcelovca90/analysis/TsneAnalyserTest.java
@@ -2,6 +2,8 @@ package io.github.marcelovca90.analysis;
 
 import static org.mockito.Mockito.when;
 
+import java.nio.file.Paths;
+
 import org.apache.commons.io.FilenameUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +19,8 @@ import weka.datagenerators.classifiers.classification.RDG1;
 @RunWith(MockitoJUnitRunner.class)
 public class TsneAnalyserTest
 {
+    private final ClassLoader classLoader = getClass().getClassLoader();
+
     @Mock
     private DatasetMetadata metadata;
 
@@ -27,10 +31,24 @@ public class TsneAnalyserTest
     public void run_withArtificialDataset_shouldNotThrowException() throws Exception
     {
         // given
-        when(metadata.getFolder()).thenReturn(FilenameUtils.separatorsToSystem("dataset/method/8"));
+        String filename = Paths.get(classLoader.getResource("dataset/method/8/data.arff").toURI()).toFile().getAbsolutePath();
+        when(metadata.getArffFilename()).thenReturn(FilenameUtils.separatorsToSystem(filename));
 
         // when
         tsneAnalyser.run(metadata, generateRandomDataset());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void run_withInexistentDataset_shouldThrowException() throws Exception
+    {
+        // given
+        String filename = Paths.get(classLoader.getResource("dataset/method/8/inexistentData.arff").toURI()).toFile().getAbsolutePath();
+        when(metadata.getArffFilename()).thenReturn(FilenameUtils.separatorsToSystem(filename));
+
+        // when
+        tsneAnalyser.run(metadata, generateRandomDataset());
+
+        // then throw exception
     }
 
     private Instances generateRandomDataset() throws Exception
